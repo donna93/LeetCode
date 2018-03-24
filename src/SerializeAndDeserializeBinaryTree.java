c8dcf4cf8a7d0f3c131fb05edb3d9f617c443902
donna93 <chocolatedonna1993@gmail.com>
2018-03-24 12:25:14 -0400

    297. Serialize and Deserialize Binary Tree
    very imortant

--- src/SerializeAndDeserializeBinaryTree.java
+++ src/SerializeAndDeserializeBinaryTree.java
@@ -1,5 +1,21 @@
 /**
- * 
+ * 297. Serialize and Deserialize Binary Tree
+ * For example, you may serialize the following tree
+        1
+      / \
+    2   3
+       / \
+      4   5
+   as "[1,2,3,null,null,4,5]"
  */
 public class SerializeAndDeserializeBinaryTree {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
 }
